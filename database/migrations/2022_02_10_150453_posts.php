cb140c0f7330d7f4419fe12d6995131e3d90d220
ToniCaimari <acaimari@cifpfbmoll.eu>
2022-02-10 16:26:33 +0100

    add: posts migration

---
+++ database/migrations/2022_02_10_150453_posts.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Posts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('titulo');
+            $table->string('extracto');
+            $table->text('contenido');
+            $table->boolean('caducable');
+            $table->boolean('comentable');
+            $table->boolean('privado');
+            $table->timestamp('last_used_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
